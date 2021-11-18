b904c813c4525417d7002c1123dd7c6ec9c551bd
Nahuel-Maiz <63472764+Nahuel-Maiz@users.noreply.github.com>
2021-11-18 16:07:46 -0300

    Primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            TP Final MercadoLibre | Desarrolado por Nahuel Maiz
+        </div>
+     );
+}
+ 
+export default Footer;
